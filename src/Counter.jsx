2fecf35c6ce9222ef61e8a1cabc649186d160a82
Gaurav <gauravsarma68@gmail.com>
2025-03-22 20:30:46 +0530

    added files

---
+++ src/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Counter App</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
+        Decrement
+      </button>
+    </div>
+  );
+}
