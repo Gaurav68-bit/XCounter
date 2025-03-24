dc93bba336a6dece5aa316b32870d689c38d7173
Gaurav <gauravsarma68@gmail.com>
2025-03-24 09:00:50 +0530

    class component counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import React, { Component } from "react";
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
+class CounterApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
 
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Counter App</h1>
-      <h2>Count: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
-        Decrement
-      </button>
-    </div>
-  );
+  increment = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
 }
+
+export default CounterApp;
+
